b66696635d1a39f56a8fd8178a4f55a4d98573f1
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-03-03 22:04:49 +0300

    Update Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,9 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: 'Kevin Birkan Beyer',
   description: "Portfolio Page",
-  icons: {
-    icon: 'favicon.png',
-  },
+
 }
 
 export default function RootLayout({ children }) {
